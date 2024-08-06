504c9d0ebe9538a712018e1450b0f686bd5d5ed1
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2024-08-06 15:30:52 +0530

    add models of the paddle ocr

--- paddle_ocr3.py
+++ paddle_ocr3.py
@@ -1,4 +1,5 @@
 import os
+
 import cv2
 from paddleocr import (
     PaddleOCR,
@@ -13,20 +14,27 @@ from PIL import Image
 SAVE_FOLDER = "./output"
 IMG_PATH = "./sample/CM-HU1157_10.png"
 FONT_PATH = "./fonts/german.ttf"
-TABLE_MODEL_DIR = "./models/table_recognition"
-LAYOUT_MODEL_DIR = "./models/layout/picodet_lcnet_x1_0_fgd_layout_table"
+
+DET_MODEL_DIR = "./models/ppstructure/inference/text_detection/ch_PP-OCRv3_det_infer"
+REC_MODEL_DIR = "./models/ppstructure/inference/text_recognition/ch_PP-OCRv3_rec_infer"
+TABLE_MODEL_DIR = "./models/ppstructure/inference/form_recognition/ch_ppstructure_mobile_v2.0_SLANet_infer"
+LAYOUT_MODEL_DIR = "./models/ppstructure/inference/layout_analysis/picodet_lcnet_x1_0_layout_infer"
+
 
 def ensure_directory_exists(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
 
 def table_detection(image_path, save_folder):
     table_engine = PPStructure(
         show_log=True,
         image_orientation=True,
         lang="en",
+        det_model_dir=DET_MODEL_DIR,
+        rec_model_dir=REC_MODEL_DIR,
         table_model_dir=TABLE_MODEL_DIR,
-        layout_model_dir=LAYOUT_MODEL_DIR
+        layout_model_dir=LAYOUT_MODEL_DIR,
     )
     img = cv2.imread(image_path)
     result = table_engine(img)
@@ -38,11 +46,13 @@ def table_detection(image_path, save_folder):
 
     return result
 
+
 def draw_table_results(image_path, result, font_path, save_folder):
     image = Image.open(image_path).convert("RGB")
     im_show = draw_structure_result(image, result, font_path=font_path)
     im_show = Image.fromarray(im_show)
     im_show.save(f"{save_folder}/table_detection.jpg")
+
 
 def text_detection(image_path, font_path, save_folder):
     ocr = PaddleOCR(use_angle_cls=True, lang="german")
@@ -62,6 +72,7 @@ def text_detection(image_path, font_path, save_folder):
     im_show = Image.fromarray(im_show)
     im_show.save(f"{save_folder}/ocr_result.jpg")
 
+
 def main():
     ensure_directory_exists(SAVE_FOLDER)
 
@@ -71,6 +82,7 @@ def main():
 
     # Text Detection
     text_detection(IMG_PATH, FONT_PATH, SAVE_FOLDER)
+
 
 if __name__ == "__main__":
     main()
