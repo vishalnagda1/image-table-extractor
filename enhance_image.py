dcca6d27dfca4ef07a1cea69263be285bffc856c
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2024-08-12 11:39:39 +0530

    reformat the image enhance script code

--- enhance_image.py
+++ enhance_image.py
@@ -1,18 +1,35 @@
-import os
-import torch
-from realesrgan import RealESRGANer
-from basicsr.archs.rrdbnet_arch import RRDBNet
 import cv2
+import torch
+from basicsr.archs.rrdbnet_arch import RRDBNet
+from realesrgan import RealESRGANer
+
 
 class ImageEnhancer:
-    def __init__(self, model_path=None, device='cuda' if torch.cuda.is_available() else 'cpu'):
+    def __init__(
+        self, model_path=None, device="cuda" if torch.cuda.is_available() else "cpu"
+    ):
         self.device = device
-        self.model_path = model_path or 'RealESRGAN_x4plus.pth'
+        self.model_path = model_path or "RealESRGAN_x4plus.pth"
         self.model = self.load_model()
 
     def load_model(self):
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-        upsampler = RealESRGANer(scale=4, model_path=self.model_path, model=model, tile=0, tile_pad=10, pre_pad=0, half=True)
+        model = RRDBNet(
+            num_in_ch=3,
+            num_out_ch=3,
+            num_feat=64,
+            num_block=23,
+            num_grow_ch=32,
+            scale=4,
+        )
+        upsampler = RealESRGANer(
+            scale=4,
+            model_path=self.model_path,
+            model=model,
+            tile=0,
+            tile_pad=10,
+            pre_pad=0,
+            half=True,
+        )
         return upsampler
 
     def enhance_image(self, input_image_path, output_image_path):
@@ -23,11 +40,12 @@ class ImageEnhancer:
         try:
             enhanced_img, _ = self.model.enhance(img, outscale=4)
         except RuntimeError as error:
-            print(f'Error in enhancing the image: {error}')
+            print(f"Error in enhancing the image: {error}")
             return
 
         # Save the enhanced image
         cv2.imwrite(output_image_path, enhanced_img)
+
 
 def main(input_image_path, output_image_path, model_path=None):
     # Initialize the enhancer
@@ -37,11 +55,11 @@ def main(input_image_path, output_image_path, model_path=None):
     enhancer.enhance_image(input_image_path, output_image_path)
     print(f"Enhanced image saved to {output_image_path}")
 
+
 if __name__ == "__main__":
     input_image_path = "sample/sample.png"
     # input_image_path = "output/transform1.png"
     output_image_path = "output/enhanced.png"
     model_path = "RealESRGAN_x4plus.pth"
-    # model_path = "ESRGAN_SRx4_DF2KOST_official-ff704c30.pth"
 
     main(input_image_path, output_image_path, model_path)
