f667878399945a15f8cac3de6d5fe4aadd017b59
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2024-08-12 13:09:38 +0530

    update image enhancer class functionality

--- image_enhancer.py
+++ image_enhancer.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import torch
 from basicsr.archs.rrdbnet_arch import RRDBNet
@@ -6,11 +8,39 @@ from realesrgan import RealESRGANer
 
 class ImageEnhancer:
     def __init__(
-        self, model_path=None, device="cuda" if torch.cuda.is_available() else "cpu"
+        self,
+        input_image_path,
+        output_directory_or_path=None,
+        model_path=None,
+        device="cuda" if torch.cuda.is_available() else "cpu",
     ):
+        self.input_image_path = input_image_path
         self.device = device
         self.model_path = model_path or "RealESRGAN_x4plus.pth"
         self.model = self.load_model()
+
+        # Determine the output directory and optional file name
+        if output_directory_or_path:
+            if os.path.isdir(output_directory_or_path):
+                self.output_directory = output_directory_or_path
+                self.output_image_name = None
+            else:
+                self.output_directory, self.output_image_name = os.path.split(
+                    output_directory_or_path
+                )
+        else:
+            self.output_directory = "./output"
+            self.output_image_name = None
+
+        # Ensure the output directory exists
+        if not os.path.exists(self.output_directory):
+            os.makedirs(self.output_directory)
+
+        self.img = cv2.imread(self.input_image_path, cv2.IMREAD_COLOR)
+        if self.img is None:
+            raise ValueError(
+                f"Image at path {self.input_image_path} could not be loaded."
+            )
 
     def load_model(self):
         model = RRDBNet(
@@ -29,37 +59,65 @@ class ImageEnhancer:
             tile_pad=10,
             pre_pad=0,
             half=True,
+            device=self.device,
         )
         return upsampler
 
-    def enhance_image(self, input_image_path, output_image_path):
-        # Load the image
-        img = cv2.imread(input_image_path, cv2.IMREAD_COLOR)
+    def enhance(self, outscale=4):
+        """
+        Enhances the image using the loaded model.
 
-        # Enhance the image
+        :param outscale: The scale factor for the enhancement.
+        :return: Self, to allow chaining.
+        """
         try:
-            enhanced_img, _ = self.model.enhance(img, outscale=4)
+            self.img, _ = self.model.enhance(self.img, outscale=outscale)
         except RuntimeError as error:
             print(f"Error in enhancing the image: {error}")
-            return
+        return self
 
-        # Save the enhanced image
-        cv2.imwrite(output_image_path, enhanced_img)
+    def save(self, output_image_name=None, format=None):
+        """
+        Saves the enhanced image with the specified output image name or falls back to the input image name.
+
+        :param output_image_name: Optional. Name to save the image as.
+        :param format: Optional. Format to save the image (e.g., 'png', 'jpg').
+        :return: The path where the image was saved.
+        """
+        # Determine the file name and format to use
+        if output_image_name:
+            file_name = output_image_name
+        elif self.output_image_name:
+            file_name = self.output_image_name
+        else:
+            file_name = os.path.basename(self.input_image_path)
+
+        # Add format extension if specified
+        if format:
+            file_name = f"{os.path.splitext(file_name)[0]}.{format}"
+
+        output_image_path = os.path.join(self.output_directory, file_name)
+
+        # Save the image
+        cv2.imwrite(output_image_path, self.img)
+        print(f"Enhanced image saved to {output_image_path}")
+
+        return output_image_path
 
 
-def main(input_image_path, output_image_path, model_path=None):
+def main(input_image_path, output_directory_or_path=None, model_path=None):
     # Initialize the enhancer
-    enhancer = ImageEnhancer(model_path=model_path)
+    enhancer = ImageEnhancer(
+        input_image_path, output_directory_or_path, model_path=model_path
+    )
 
-    # Enhance the image
-    enhancer.enhance_image(input_image_path, output_image_path)
-    print(f"Enhanced image saved to {output_image_path}")
+    # Enhance the image and save
+    enhancer.enhance().save()
 
 
 if __name__ == "__main__":
     input_image_path = "sample/sample.png"
-    # input_image_path = "output/transform1.png"
-    output_image_path = "output/enhanced.png"
+    output_directory_or_path = "output/enhanced.png"
     model_path = "RealESRGAN_x4plus.pth"
 
-    main(input_image_path, output_image_path, model_path)
+    main(input_image_path, output_directory_or_path, model_path)
